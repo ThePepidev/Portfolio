848da853dd423db33a60c928ec2bdfc797b48032
ThePepidev <mathys1.dupont@epitech.eu>
2025-08-12 12:22:33 +0200

    fix(main.jsx): fixing analytics integration

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Analytics } from "@vercel/analytics/next"
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import Portfolio from "./pages/Portfolio.js";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Portfolio />
+    <Analytics />
   </React.StrictMode>
 );
