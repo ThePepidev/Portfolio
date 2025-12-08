79ee0e61e906989d8afc8700cad7085099ebb401
ThePepidev <mathys1.dupont@epitech.eu>
2025-12-08 13:44:47 +0100

    fix(vite.config.js): fixing source map exposing

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
   },
   build: {
     outDir: "dist",
-    sourcemap: true,
+    sourcemap: false,
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
